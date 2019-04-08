ffc4cb8fb1aab379f1573d94c072d7f780b11c18
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-04-08 17:44:43 +0700

    Finished (?) API

--- api/dept.js
+++ api/dept.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
 const sql = require("../config/sql-server");
 
-router.get("/", async (req, res) => {
+router.get("/", async (_, res) => {
   const deptList = (await sql.query("select * from PB")).recordsets[0];
   res.send(deptList);
+});
+
+router.post("/", async (req, res) => {
+  const { MSPB, TENPB } = req.body;
+  const query = `
+    insert into PB
+    output inserted.MSPB, inserted.TENPB
+    values (${MSPB}, N'${TENPB}')`;
+  const obj = (await sql.query(query)).recordsets[0][0];
+  res.send(obj);
 });
 
 module.exports = router;
--- api/staff.js
+++ api/staff.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
 const sql = require("../config/sql-server");
 
-router.get("/", async (req, res) => {
+router.get("/", async (_, res) => {
   const staffList = (await sql.query("select * from NV")).recordsets[0];
   res.send(staffList);
+});
+
+router.post("/", async (req, res) => {
+  const { MSNV, HT, GT, MSPB } = req.body;
+  const query = `
+    insert into NV
+    output inserted.MSNV, inserted.HT, inserted.GT, inserted.MSPB
+    values ('${MSNV}', N'${HT}', ${GT}, ${MSPB})`;
+  const obj = (await sql.query(query)).recordsets[0][0];
+  res.send(obj);
 });
 
 module.exports = router;
